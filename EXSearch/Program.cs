05f6b6834b78dc00186c52214ccd759668e48c62
Marcus André <ddtank.marcus@gmail.com>
2018-02-17 18:34:34 -0200

    Initial commit

---
+++ EXSearch/Program.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace EXSearch {
+    class Program {
+
+        static string Log = null;
+        static string Filter = "*.*";
+        static void Main(string[] args) {
+            Console.Title = "Encoding X Searcher - Encoding Discovery Tool";
+            string Text, Path;
+            ParseArgs(args, out Text, out Path);
+
+            string[] Files = PathWork(Path);
+
+            StringBuilder SB = new StringBuilder();
+            SB.AppendLine("Encoding X Search - Result List");
+
+            foreach (string File in Files) {
+                Console.WriteLine($"Searching inside the \"{System.IO.Path.GetFileName(File)}\"");
+                Stream Reader = new StreamReader(File).BaseStream;
+                byte[] Buffer = new byte[Text.Length * 2];
+                long Ptr = Reader.Position;
+                while (Reader.Read(Buffer, 0, Buffer.Length) > 0) {
+                    Reader.Position = ++Ptr;
+                    if (TestAt(Buffer, Text) || TestAtUni(Buffer, Text)) {
+                        Console.WriteLine($"Match At 0x{Ptr - 1:X8}");
+                        SB.AppendLine($"Match at 0x{Ptr - 1:X8} in the {System.IO.Path.GetFileName(File)}");
+                    }
+                }
+                Reader.Close();
+            }
+
+            if (Log != null) {
+                Console.WriteLine("Generating Log File...");
+                File.WriteAllText(Log, SB.ToString());
+            }
+
+            Console.WriteLine("Press a Key To Exit...");
+            Console.ReadKey();
+            Console.WriteLine();
+        }
+        private static bool TestAt(byte[] Data,string Text) {
+            Dictionary<char, ushort> CharMap = new Dictionary<char, ushort>();
+            try {
+                for (int i = 0; i < Text.Length; i++) {
+                    byte b = Data[i];
+                    char c = Text[i];
+                    if (CharMap.ContainsKey(c)) {
+                        if (b != CharMap[c])
+                            return false;
+                    } else {
+                        if (CharMap.ContainsValue(b))
+                            return false;
+                        CharMap.Add(c, b);
+                    }
+                }
+            } catch {
+                return false;
+            }
+
+            return true;
+        }
+        private static bool TestAtUni(byte[] Data, string Text) {
+            Dictionary<char, ushort> CharMap = new Dictionary<char, ushort>();
+
+            try {
+                for (int i = 0; i < Text.Length; i++) {
+                    ushort b = BitConverter.ToUInt16(new byte[] { Data[(i*2) + 1], Data[i*2] }, 0);
+                    char c = Text[i];
+                    if (CharMap.ContainsKey(c)) {
+                        if (b != CharMap[c])
+                            return false;
+                    } else {
+                        if (CharMap.ContainsValue(b))
+                            return false;
+                        CharMap.Add(c, b);
+                    }
+                }
+            } catch {
+                return false;
+            }
+
+            return true;
+        }
+        
+        private static string[] PathWork(string path) {
+            if (File.Exists(path))
+                return new string[] { path };
+
+            if (Directory.Exists(path))
+                return Directory.GetFiles(path, Filter, SearchOption.AllDirectories);
+
+            throw new FileNotFoundException("File/Directory not found", path);
+        }
+
+        private static void ParseArgs(string[] Args, out string Text, out string Path) {
+            Text = string.Empty;
+            Path = string.Empty;
+
+            for (int i = 0; i < Args.Length; i++) {
+                string Arg = Args[i].TrimStart('-', '\\', '/');
+                switch (Arg.ToLower()) {
+                    case "t":
+                    case "text":
+                    case "line":
+                        Text = Args[++i];
+                        break;
+                    case "p":
+                    case "path":
+                    case "directory":
+                    case "file":
+                    case "inside":
+                        Path = Args[++i];
+                        break;
+                    case "f":
+                    case "filter":
+                    case "extension":
+                    case "ext":
+                        Filter = Args[++i];
+                        break;
+                    case "l":
+                    case "log":
+                        Log = Args[++i];
+                        break;
+                    case "about":
+                    case "help":
+                    case "?":
+                        Console.WriteLine("EXSearch - Encoding Discovery Tool");
+                        Console.WriteLine("This tool can search text using an unknown encoding, for it to work you need to use a line that contains repetitions of a same letter in a non sequencial form, the more repetitions you have more precise will be the result, the tool even if not supporting cryptography can too be used to search text in \"encrypted\" files by a simple XOR of one byte");
+
+                        Console.WriteLine();
+
+                        Console.WriteLine("Usage:");
+                        Console.WriteLine("\tEXSearch -t \"Sample compatible text\" -p \"C:\\FilesDir\\\" -f \"*.bin\" -l \"Results.log\"");
+
+                        Console.WriteLine("Press a Key to Exit");
+
+                        Console.ReadKey();
+                        Console.WriteLine();
+
+                        Process.GetCurrentProcess().Kill();
+                        break;
+                }
+            }
+
+
+            if (string.IsNullOrWhiteSpace(Text)) {
+                Console.WriteLine("Type a Text:");
+                Text = Console.ReadLine();
+            }
+
+            if (string.IsNullOrWhiteSpace(Path)) {
+                Console.WriteLine("Type a path to search:");
+                Path = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,34 @@
+﻿# Encoding X Searcher
+
+This tool can search text using an unknown coding, for it to work you need to use a line that contains repetitions of a same letter in a non sequencial form, the more repetitions you have more precise will be the result, the tool even if not supporting cryptography can too be used to search text in "crypted" files by a simple XOR of one byte
+
+# Strategy
+
+The search use the character repetion and serach for a repetion with the same pattern of repetions
+for example:
+***"This is just a sample text"***
+can match with
+***"Yjod od kidy s dsqzçr yrcy"***
+He matchs because the repetion pattern have the same distance and times in this line
+
+- **T** and **Y**: Shows only one time at the position **0**
+- **h** and **j**: Shows only one time at the position **1**
+- **i** and **o**: Shows at the positions: **[2, 5]**
+- **s** and **d**: Shows  at the positions: **[3, 6, 10, 15]**
+- **j** and **k**: Shows only one time at the position **8**
+- **u** and **i**: Shows only one time at the position **9**
+- **t** and **y**: Shows at the positions: **[11, 22, 25]**
+- **a** and **s**: Shows at the positions: **[13, 16]**
+- **m** and **q**: Shows only one time at the position **17**
+- **p** and **z**: Shows only one time at the position **18**
+- **l** and **ç**: Shows only one time at the position **19**
+- **e** and **r**: Shows at the positions: **[20, 23]**
+- **x** and **c**: Shows only one time at the position **24**
+
+And if all pattern match he will list the file and the match offset, If the file use a simple obfuscation like a static xor this tool will works too.
+But keep in mind, strings like this:
+"Nothing Here"
+Only contains the repetion in the 'e', this will a false positive match's, you need choose a good string to a proper search.
+
+## Why use this?
+I created this tool to search the text inside game scripts who uses a personal encoding, since i don't know where is the text i just found a good string and give to this tool search, he will say where the string matchs and I can analyze the game encoding just putting a hardware breakpoint in the text.
